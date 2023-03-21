342da31bc7593c5acd067a0b7131b779b10461f0
Thiện Phạm <phamquangthien99@gmai.com>
2023-03-21 16:59:30 +0700

    add product page

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
+import axios from 'axios'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 createApp(App).use(store).use(router).mount('#app')
+axios.defaults.baseURL = 'http://127.0.0.1:8000/'
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,64 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Welcome to the PC shop
+        </p>
+        <p class="subtitle">
+          The best PC store online
+        </p>
+      </div>
+    </section>
+    <div class="columns is-multiline">
+      <div class="column is-12">
+          <h2 class="is-size-2 has-text-centered">Latest products</h2>
+      </div>
+      <div 
+      class="colums is-3"
+      v-for="product in latestProducts"
+      v-bind:key="product.id"
+      >
+        <div class="box">
+          <figure class="image mb-4">
+            <img v-bind:src="product.get_thumbnail">
+          </figure>
+          <h3 class="is-size-4">{{ product.name }}</h3>
+          <p class="is-size-6 has-text-grey">{{ product.price }} Tr </p>
+          
+          <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View detais</router-link>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      latestProducts: []
+    }
+  },
+  mounted() {
+    this.getLatestProducts()
+    console.log(this.latestProducts.get_absolute_url)
+  },
+  methods: {
+    getLatestProducts() {
+      axios.get('/api/v1/latest-products/')
+      .then(response => {
+        this.latestProducts = response.data
+        console.log(this.latestProducts)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+  },
 }
 </script>
