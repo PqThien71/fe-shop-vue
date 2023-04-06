ba3c4001361c192065bf595bddec399e4ee00c68
Thiện Phạm <phamquangthien99@gmai.com>
2023-04-06 10:11:07 +0700

    show order in acc-page

--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -11,14 +11,14 @@
 
             <hr>
 
-            <!-- <div class="column is-12">
+            <div class="column is-12">
                 <h2 class="subtitle">My orders</h2>
 
                 <OrderSummary
                     v-for="order in orders"
                     v-bind:key="order.id"
                     v-bind:order="order" />
-            </div>-->
+            </div>
         </div>
     </div>
 
@@ -26,10 +26,19 @@
 
 <script>
 import axios from 'axios'
+
+import OrderSummary from '../components/OrderSummary.vue'
 export default{
     name: "MyAccount",
+    components: {
+        OrderSummary
+    },
+    mounted(){
+        this.getMyOrders()
+    },
     data(){
         return{
+            orders:[]
         }
     },
     methods:{
@@ -40,6 +49,16 @@ export default{
             localStorage.removeItem('userid')
             this.$store.commit('removeToken')
             this.$router.push('/')
+        },
+        getMyOrders(){
+            axios
+            .get('api/v1/orders/')
+            .then(response => {
+                this.orders = response.data
+            })
+            .catch(error => {
+                console.log(error)
+            })
         }
     },
 }
